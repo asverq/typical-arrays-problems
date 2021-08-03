9d0b995d9ad318ff9af01425acfbd735f4b61ef0
Igor Arinichev <igor.arin@gmail.com>
2021-08-03 19:52:10 +0300

    feat: implement min, max & avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (!array || array.length == 0) return 0;
+    array.sort(num);
+    return array[0];
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (!array || array.length == 0) return 0;
+    array.sort(num);
+    return array[array.length - 1];
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (!array || array.length == 0) return 0;
+    return (array.reduce((a, b) => a + b, 0) / 2) / 10;
+}
+
+function num(a, b) {
+    if (a > b) return 1;
+    if (a == b) return 0;
+    if (a < b) return -1;
 }
